25092622af615c049a14edc3a0f15235c2c4f2d5
PedroMarquesSa <pedro.marques.sa@nos.pt>
2025-12-16 16:20:49 +0000

    refactor: reorganize data loading and cleaning functions

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,16 +1,19 @@
 """Tests for the cleaning module"""
+
 import pandas as pd
 
-from life_expectancy.cleaning import clean_data
+from life_expectancy.cleaning import load_data, clean_data, save_data
 from . import OUTPUT_DIR
 
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data(country="PT")
-    pt_life_expectancy_actual = pd.read_csv(
-        OUTPUT_DIR / "pt_life_expectancy.csv"
-    )
+    data = load_data()
+    cleaned_data = clean_data(data, country="PT")
+    output_file = OUTPUT_DIR / "pt_life_expectancy.csv"
+    save_data(cleaned_data, country="PT", output_file=output_file)
+
+    pt_life_expectancy_actual = pd.read_csv(output_file)
     pd.testing.assert_frame_equal(
         pt_life_expectancy_actual, pt_life_expectancy_expected
     )
